338b4c81c38733a764c8a2cd49c11906d7bb0597
Cihat Gündüz <commits@cihatguenduez.de>
2021-01-29 17:24:37 +0100

    Add SwiftPM package manifest

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "KVOController",
+    platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)],
+    products: [.library(name: "KVOController", targets: ["KVOController"])],
+    targets: [
+        .target(
+            name: "KVOController",
+            path: "FBKVOController",
+            exclude: ["Info.plist"],
+            publicHeadersPath: "include"
+        )
+    ]
+)
